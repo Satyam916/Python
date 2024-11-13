3096227ecd197fbd38c81ec7d037e7c278d2f8bc
Satyam Verma <satyamverma4444@gmail.com>
2024-11-13 18:03:14 +0530

    python

--- prac_assignment.py
+++ prac_assignment.py
@@ -1,4 +1,10 @@
-print('Hello World')
-x=10
-y=5
-print(x)
+msg= 'Hello World'
+
+print(msg.find('l'))
+# print(dir(msg))
+courses=['History','Math','Science','ComSci']
+course_str= ' - '.join(courses)
+print(course_str)
+
+course_list= course_str.split(' - ')
+print(course_list)
